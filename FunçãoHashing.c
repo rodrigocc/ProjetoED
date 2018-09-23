11c8aca413270849dd3948a9369047c5191e32fb
Rodrigo Araujo <rodrigoasilvacc@gmail.com>
2018-09-23 19:38:18 -0300

    Add files via upload
    Começo da Função Hashing , transformando String em Chave inteira

---
+++ FunçãoHashing.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+//
+// Created by rodrigo on 23/09/18.
+//
+int ChaveString(//No caso receberia o arquivo dicionario tipo Arquivo /x variável){
+int i;
+int valor = 7;
+int tamanho = strlen(//TAMANHO DO DICIONÁRIO);
+for(i =0 ; i < tamanho; i++){
+    valor = 31 * valor + (int) //valor ASCII da variável que Guarda o Arquivo com o Dicionário[i];
+
+
+}
+return valor;
+)
+
+
+}
