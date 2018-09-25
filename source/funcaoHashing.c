e8221681ab772c146b25d347225a888f84970c1c
Rodrigo Araujo <rodrigoasilvacc@gmail.com>
2018-09-25 10:14:51 -0300

    Update funcaoHashing.c

--- source/funcaoHashing.c
+++ source/funcaoHashing.c
@@ -21,8 +21,26 @@ int ChaveString(char *str, int tamanhoDeBucktes){
 
 
 int hash(char * str, int tamanhoBucktes){
+	int i, posicaoHash;
+	int chave = 7
+	int tamanho = strlen(str);
+
+	for(i = 0; i < tamanho; i++){
+		chave = 31 * chave +(int);
+		posicaoHash = (chave % tamanhoBuckets);
+	}
+	return posicaoHash;
 
 	
 
 
 }
+
+int MultiplicaPosicaoHash(//Seria o Hash Gerado Pela FunçãoHash// int tamanhoBuckets){
+	float A = 0.718033
+	float val = //hASH * A;
+	val = val - (int)val;
+	return(int) (tamanhoBuckets * val);
+
+}
+
